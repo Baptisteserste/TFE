c50faca0ef1d5a173a1bbb76870931ad4d2814a0
Baptiste <baptiste.serste@gmail.com>
2026-03-22 17:19:11 +0100

    feat: ajout route temporaire pour migration depuis le navigateur

--- van-app-backend/routes/web.php
+++ van-app-backend/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json(['message' => 'VanApp API is running']);
 });
+
+Route::get('/migrate-db', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+        return response()->json(['message' => 'Base de donnees migree avec succes !', 'output' => \Illuminate\Support\Facades\Artisan::output()]);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()]);
+    }
+});
